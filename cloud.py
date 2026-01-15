55b3d073c17fefcce8dfb57fc2d8f8591ec7f6bf
pranjalchavancomp2023-source <pranjal.chavan.comp.2023@vpkbiet.org>
2026-01-15 16:45:50 +0530

    Update cloud.py

--- cloud.py
+++ cloud.py
@@ -8,8 +8,8 @@ storage = float(input("Enter storage used (in GB): "))
 hours_used = float(input("Enter compute hours used: "))
 
 # Cost rates (example values)
-storage_cost_per_gb = 10    # ₹5 per GB
-compute_cost_per_hour = 15  # ₹10 per hour
+storage_cost_per_gb = 29    # ₹5 per GB
+compute_cost_per_hour = 67  # ₹10 per hour
 
 # Calculate total cost
 total_storage_cost = storage * storage_cost_per_gb
