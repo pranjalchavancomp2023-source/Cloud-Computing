29eec61676114ee00294b627dc6ede326fb8f598
pranjalchavancomp2023-source <pranjal.chavan.comp.2023@vpkbiet.org>
2026-01-15 15:43:15 +0530

    Initial commit

--- cloud.py
+++ cloud.py
@@ -1,19 +1,19 @@
 # Cloud Computing Assignment
 # Simple Cloud Service Cost Calculator
 
-print("Cloud Service Cost Calculator")
+print("Calculating Cloud Service Cost")
 
 # Input from user
-storage_gb = float(input("Enter storage used (in GB): "))
-compute_hours = float(input("Enter compute hours used: "))
+storage = float(input("Enter storage used (in GB): "))
+hours_used = float(input("Enter compute hours used: "))
 
 # Cost rates (example values)
-storage_cost_per_gb = 5     # ₹5 per GB
-compute_cost_per_hour = 10  # ₹10 per hour
+storage_cost_per_gb = 10    # ₹5 per GB
+compute_cost_per_hour = 15  # ₹10 per hour
 
 # Calculate total cost
-total_storage_cost = storage_gb * storage_cost_per_gb
-total_compute_cost = compute_hours * compute_cost_per_hour
+total_storage_cost = storage * storage_cost_per_gb
+total_compute_cost = hours_used * compute_cost_per_hour
 total_cost = total_storage_cost + total_compute_cost
 
 # Display output
